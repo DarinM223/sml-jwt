7794702a8319b10b067937dd382e539c24b8ba6e
D M <darinm223@gmail.com>
2023-09-17 06:55:26 -0700

    Finish JWT get grants for MLKit

--- mlkit-stubs.c
+++ mlkit-stubs.c
@@ -1,4 +1,5 @@
 #include <jwt.h>
+#include <errno.h>
 #include "./mlkit/Runtime/List.h"
 #include "./mlkit/Runtime/String.h"
 #include "./mlkit/Runtime/Exception.h"
@@ -29,4 +30,80 @@ uintptr_t c_jwt_show(Region strRho, Context ctx, uintptr_t jwt, uintptr_t exn)
   String mlResult = convertStringToML(strRho, result);
   free((void *)result);
   return (uintptr_t)mlResult;
+}
+
+uintptr_t c_jwt_get_grant(Region strRho, Context ctx, uintptr_t jwt, String key, uintptr_t exn)
+{
+  char cKey[1000];
+  convertStringToC(ctx, key, cKey, 1000, exn);
+  const char *result = jwt_get_grant((jwt_t *)jwt, cKey);
+  if (result == NULL)
+  {
+    raise_exn(ctx, exn);
+    return 0;
+  }
+
+  String mlResult = convertStringToML(strRho, result);
+  return (uintptr_t)mlResult;
+}
+
+long c_jwt_get_grant_int(Context ctx, uintptr_t jwt, String key, uintptr_t exn)
+{
+  char cKey[1000];
+  convertStringToC(ctx, key, cKey, 1000, exn);
+  long result = jwt_get_grant_int((jwt_t *)jwt, cKey);
+  if (errno == ENOENT)
+  {
+    raise_exn(ctx, exn);
+    return 0;
+  }
+  return convertIntToML(result);
+}
+
+int c_jwt_get_grant_bool(Context ctx, uintptr_t jwt, String key, uintptr_t exn)
+{
+  char cKey[1000];
+  convertStringToC(ctx, key, cKey, 1000, exn);
+  int result = jwt_get_grant_bool((jwt_t *)jwt, cKey);
+  if (errno == ENOENT)
+  {
+    raise_exn(ctx, exn);
+    return 0;
+  }
+  return convertBoolToML(result);
+}
+
+uintptr_t c_jwt_get_grants_json(Region strRho, Context ctx, uintptr_t jwt, String key, uintptr_t exn)
+{
+  char *result = NULL;
+  if (key == NULL)
+  {
+    result = jwt_get_grants_json((jwt_t *)jwt, NULL);
+  }
+  else
+  {
+    char cKey[1000];
+    convertStringToC(ctx, key, cKey, 1000, exn);
+    result = jwt_get_grants_json((jwt_t *)jwt, cKey);
+  }
+  if (result == NULL)
+  {
+    raise_exn(ctx, exn);
+    return 0;
+  }
+  String mlResult = convertStringToML(strRho, result);
+  return (uintptr_t)mlResult;
+}
+
+void c_jwt_add_grant(Context ctx, uintptr_t jwt, String key, String value, uintptr_t exn)
+{
+  char cKey[1000];
+  convertStringToC(ctx, key, cKey, 1000, exn);
+  char cValue[1000];
+  convertStringToC(ctx, value, cValue, 1000, exn);
+  int result = jwt_add_grant((jwt_t *)jwt, cKey, cValue);
+  if (result != 0)
+  {
+    raise_exn(ctx, exn);
+  }
 }
--- sml-jwt.mlkit.sml
+++ sml-jwt.mlkit.sml
@@ -13,8 +13,47 @@ struct
   fun show t : string =
     prim ("c_jwt_show", (getCtx (), t, Exn))
     handle Exn => raise (JwtError ("show", ~1))
+  fun getGrant t key : string option =
+    SOME (prim ("c_jwt_get_grant", (getCtx (), t, key, Exn)))
+    handle Exn => NONE
+  fun getGrantInt t key : int option =
+    SOME (prim ("c_jwt_get_grant_int", (getCtx (), t, key, Exn)))
+    handle Exn => NONE
+  fun getGrantBool t key : bool option =
+    SOME (prim ("c_jwt_get_grant_bool", (getCtx (), t, key, Exn)))
+    handle Exn => NONE
+  fun getGrantsJson t key : string option =
+    let
+      val key =
+        case key of
+          SOME (Key key) => key
+        | NONE => prim ("sml_null", ())
+    in
+      SOME (prim ("c_jwt_get_grants_json", (getCtx (), t, key, Exn)))
+    end
+    handle Exn => NONE
+  fun addGrant t key value : unit =
+    prim ("c_jwt_add_grant", (getCtx (), t, key, value, Exn))
+    handle Exn => raise (JwtError ("addGrant", ~1))
+end
+
+local
+  fun showOption f (SOME s) = "SOME " ^ f s
+    | showOption _ NONE = "NONE"
+in
+  val showStr_option = showOption (fn t0 => "\"" ^ t0 ^ "\"")
+  val showInt_option = showOption Int.toString
+  val showBool_option = showOption Bool.toString
+  val showAlgorithm_option = showOption AlgUtils.showAlgorithm
 end
 
 val jwt: Jwt.t = Jwt.create ()
 val () = print (Jwt.show jwt ^ "\n")
+val () = Jwt.addGrant jwt "hello" "world"
+val () = print (showStr_option (Jwt.getGrant jwt "hello") ^ "\n")
+val () = print (showInt_option (Jwt.getGrantInt jwt "foo") ^ "\n")
+val () = print (showBool_option (Jwt.getGrantBool jwt "bar") ^ "\n")
+val () = print
+  (showStr_option (Jwt.getGrantsJson jwt (SOME (Jwt.Key "hello"))) ^ "\n")
+val () = print (showStr_option (Jwt.getGrantsJson jwt NONE) ^ "\n")
 val () = Jwt.free jwt
